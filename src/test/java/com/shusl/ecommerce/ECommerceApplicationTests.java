a23390c11e87084513c1ef60b163a63c5617d3b9
shusl <2825278255@qq.com>
2024-01-06 19:31:06 +0800

    初始化项目

---
+++ src/test/java/com/shusl/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shusl.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
